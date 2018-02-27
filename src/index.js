e9c6f4d2dceadd1c0dfd8cace9fdd965ba8831cf
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-02-27 10:36:53 -0600

    Add bootstrap.css files to project entry point.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
