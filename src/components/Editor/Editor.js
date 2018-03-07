6839e1cb7816043f0f89afc7008d12b9fa86d487
Larson <rlrson@uiowa.edu>
2018-03-06 18:09:33 -0600

    Add zooming function to our editor.

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -5,6 +5,8 @@ export default class Editor extends Component {
   constructor() {
     super();
     this.state = { width: 500, height: 500 };
+
+    this.canvasZoom = this.canvasZoom.bind(this);
   }
 
   componentDidMount() {
@@ -14,11 +16,39 @@ export default class Editor extends Component {
     this.setState({ width: container.clientWidth });
   }
 
+  canvasZoom(event) {
+    /*
+     * @method
+     * @description Method to handle zooming in on our canvas.
+     */
+    event.evt.preventDefault();
+
+    let canvas = event.currentTarget,
+      old_scale = canvas.scaleX();
+
+    var mouse_pos_to = {
+      x: canvas.getPointerPosition().x / old_scale - canvas.x() / old_scale,
+      y: canvas.getPointerPosition().y / old_scale - canvas.y() / old_scale,
+    };
+
+    let new_scale = event.evt.deltaY < 0 ? old_scale * 1.1 : old_scale / 1.1;
+    canvas.scale({ x: new_scale, y: new_scale });
+
+    var new_pos = {
+      x: -(mouse_pos_to.x - canvas.getPointerPosition().x / new_scale) * new_scale,
+      y: -(mouse_pos_to.y - canvas.getPointerPosition().y / new_scale) * new_scale
+    };
+
+    canvas.position(new_pos);
+    canvas.batchDraw();
+  }
+
   render() {
     return (
       <Stage
         width={this.state.width}
         height={this.state.height}
+        onWheel={this.canvasZoom}
         onContentClick ={this.props.onClick}
         onContentDblClick={this.props.onContentClick}
         onDragEnd={this.props.handleDragEnd}
