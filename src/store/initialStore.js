91be35034b32db1f6c78c818498e7e379e4c576b
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-03-09 12:11:31 -0600

    Add syntax corrections to initialState.

--- src/store/initialStore.js
+++ src/store/initialStore.js
@@ -2,34 +2,34 @@
  * Initial State
  */
 
- export const initialState = {
-   // Holds our form data, including selected furniture type (toolbar)
-   forms: {
+export const initialState = {
+// Holds our form data, including selected furniture type (toolbar)
+  forms: {
     eventName: '',
     eventDate: '',
     eventTime: '08:00',
     chairsPerTable: 6,
-    EventComments: '',
+    eventComments: '',
     userEmail: ''
-   },
-   // Keep a only-incremental counter to uniquely name our canvas elements.
-   furn_ids = {
+  },
+  // Keep a only-incremental counter to uniquely name our canvas elements.
+  furn_ids: {
     circle: 0,
     rect: 0,
     bar: 0,
     poster: 0,
     trash: 0
-   },
-   // Hold the furniture items in a quick lookup
-   furn_items = {
-     circle: {},
-     rect: {},
-     bar: {},
-     poster: {},
-     trash: {}
-   },
-   // Keep track of our drag and drop editor.
-   editor: {
+  },
+  // Hold the furniture items in a quick lookup
+  furn_items: {
+    circle: {},
+    rect: {},
+    bar: {},
+    poster: {},
+    trash: {}
+  },
+  // Keep track of our drag and drop editor.
+  editor: {
     width: 500,
     height: 500,
     xy: [0, 0],
@@ -39,15 +39,15 @@
     focusedFurn: null,      // Selected furniture in editor, for transforms
     canvasRef: null,        // Our Konva stage reference
     URI: ''                 // Our canvas snapshot
-   },
-   // The numbers important for event coordinators.
-   calculated: {
-     numChairs: 0,
-     numChairCarts: 0,
-     numCircleCarts: 0,
-     numRectCarts: 0,
-     numBarCarts: 0,
-     numPosters: 0,
-     numTrashs: 0
-   }
- }
+  },
+  // The numbers important for event coordinators.
+  calculated: {
+    numChairs: 0,
+    numChairCarts: 0,
+    numCircleCarts: 0,
+    numRectCarts: 0,
+    numBarCarts: 0,
+    numPosters: 0,
+    numTrashs: 0
+  }
+}
