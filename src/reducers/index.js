b594786695163bd53e55d050ed3098bb73f85ab5
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-03-10 13:31:40 -0600

    Add index to reducers to wrap them all for exports.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,13 @@
  */
 
 import { combineReducers } from 'redux';
+import { initialState } from '../store/initialStore';
 import formReducer from './form.reducer';
 import idReducer from './id.reducer';
+import itemReducer from './item.reducer';
 
 export default combineReducers({
   formReducer,
-  idReducer
+  idReducer,
+  itemReducer
 })
