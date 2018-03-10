c9dd50cab0da22c72e8ba1c1bd24ac7225fb96f4
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-03-10 13:34:41 -0600

    Light dispatching added to make sure our app is behaving.

--- src/index.js
+++ src/index.js
@@ -2,31 +2,66 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+// Inital state
+import { initialState } from './store/initialStore';
 // reducer
-import reducer from './reducers';
-
-// actions fro middleware
+import reducers from './reducers';
+// actions
 
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 // create store
 const store = createStore(
-  reducer,
+  reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 store.dispatch({type: 'UPD_FORM', name: 'eventName', value: 'test name'});
-store.dispatch({type: 'INC_FURN_ID', value: 'circle'});
+store.dispatch({type: 'UPD_FORM', name: 'chairsPerTable', value: '8'});
+//store.dispatch({type: 'INC_FURN_ID', value: 'circle'});
+store.dispatch({
+  type: 'ADD_FURN_ITEM', 
+  furn_type: 'circle',
+  x: 50,
+  y: 50
+});
+store.dispatch({
+  type: 'UPD_FURN_ITEM',
+  item_id: 'circle0',
+  furn_type: 'circle',
+  x: 150,
+  y: 150
+});
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+store.dispatch({
+  type: 'ADD_FURN_ITEM', 
+  furn_type: 'circle',
+  x: 0,
+  y: 0
+});
+
+store.dispatch({
+  type: 'ADD_FURN_ITEM', 
+  furn_type: 'circle',
+  x: Math.random() * 100,
+  y: Math.random() * 100
+});
+
+store.dispatch({
+  type: 'RM_FURN_ITEM',
+  furn_type: 'circle',
+  item_id: 'circle1'
+});
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
 );
-
 registerServiceWorker();
