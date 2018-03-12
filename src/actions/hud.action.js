99e13427a6c0c9ec879481f324712a2cf874780d
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-03-11 23:01:47 -0500

    Remove uneeded HUD actions.

--- src/actions/hud.action.js
+++
@@ -1,6 +0,0 @@
-/* Actions to trigger an update to our calculated variables. */
-import { hudActions } from '../constants/actionTypes';
-
-export const updateHUD = () => ({
-  type: hudActions.UPD_HUD
-});
