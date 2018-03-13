75cc25d2e7c494d5123d2498863a986cc09fb915
Larson <rlrson@uiowa.edu>
2018-03-12 19:59:24 -0500

    Add more buttons to toolbar, prepare for linking it to our store.

--- src/components/editor/toolbar.component.js
+++ src/components/editor/toolbar.component.js
@@ -2,15 +2,46 @@ import React from 'react';
 import {Button, ButtonGroup, ButtonToolbar} from 'reactstrap';
 
 const Toolbar = (props) => {
+  console.log(props);
   return (
     <ButtonToolbar>
-      <ButtonGroup>
-        <Button>Circle</Button>
-        <Button>Rect</Button>
-        <Button>Bar</Button>
-        <Button>Poster</Button>
-        <Button>Trash</Button>
+      <ButtonGroup className="float-left">
+        <Button 
+          active={props.selectedFurnType === 'circle'}
+          color={props.selectedFurnType === 'circle' ? 'dark' : 'light'}
+          size="sm"
+        >
+          Circle
+        </Button>
+        <Button
+          active={props.selectedFurnType === 'rect'}
+          size="sm"
+        >
+          Rect
+        </Button>
+        <Button
+          active={props.selectedFurnType === 'rect'}
+          size="sm"
+        >
+          Bar
+        </Button>
+        <Button
+          active={props.selectedFurnType === 'rect'}
+          size="sm"
+        >Poster</Button>
+        <Button
+          active={props.selectedFurnType === 'rect'}
+          size="sm"
+        >Trash</Button>
       </ButtonGroup>
+
+      <ButtonGroup className="float-right">
+        <Button 
+          size="sm"
+        >6</Button>
+        <Button size="sm">8</Button>
+      </ButtonGroup>
+
     </ButtonToolbar>
   );
 }
