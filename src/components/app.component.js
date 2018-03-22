7fb80d26a0b5325679d2c57027e6e5ced0403c56
Larson <rlrson@uiowa.edu>
2018-03-22 16:44:04 -0500

    Converted App component.

--- src/components/app.component.js
+++ src/components/app.component.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import { Container, Row, Col, Navbar, NavbarBrand } from 'reactstrap';
+import { Fabric, CommandBar } from 'office-ui-fabric-react';
 import { FormContainer, ToolbarContainer, HudContainer, GuiContainer } from '../containers';
 
 const App = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Navbar color="black" dark>
-          <NavbarBrand>
-            <strong>Create an Event &nbsp; &nbsp;</strong>
-            College of Public Health
-          </NavbarBrand>
-        </Navbar>
-      </Row>
-      <Row>
-        <Col xs={12} sm={6} lg={4}>
-          <FormContainer />
-        </Col>
-        <Col xs={12} sm={6} lg={8}>
-          <ToolbarContainer/>
-          <HudContainer/>
-          <GuiContainer/>
-        </Col>
-      </Row>
-    </Container>
+    <Fabric className="App ms-normalize">
+      <div className="ms-Grid">
+
+        {/* Navigation bar + header */}
+        <div className="ms-Grid-row">
+          <CommandBar />
+        </div>
+
+        {/* Application */}
+        <div className="ms-Grid-row">
+          <div className="ms-Grid-col ms-sm12 ms-md6 ms-lg4">
+            <FormContainer />
+          </div>
+
+          <div className="ms-Grid-col ms-sm12 ms-md6 ms-lg8">
+            <ToolbarContainer />
+            <HudContainer />
+            <GuiContainer />
+          </div>
+        </div>
+      </div>
+    </Fabric>
   );
 }
 
