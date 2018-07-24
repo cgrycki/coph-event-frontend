efe15d61401768430ed820b8b65c23a785cf8715
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-24 16:31:55 -0500

    Update login actions and reducer

--- src/actions/app.actions.js
+++ src/actions/app.actions.js
@@ -6,13 +6,56 @@ import * as rp from 'request-promise';
 
 
 /**
- * Updates our application's user login status
- * @param {boolean} value Indicates if a user is logged into a server session 
+ * Notifies our application we're attempting to validate logged-in status
  */
-export const updateLogin = (value) => ({
-  type : appActions.UPDATE_LOGIN,
-  value: value
+const fetchLoginLoading = () => ({
+  type: appActions.LOGIN_LOADING
 })
+
+
+/**
+ * Sends an action with the successful response of the API call
+ * @param {*} response 
+ */
+const fetchLoginSuccess = (response) => ({
+  type: appActions.LOGIN_SUCESS,
+  payload: response
+})
+
+
+/**
+ * Creates an action to pass our store an error returned from the login validation
+ * @param {*} error 
+ */
+const fetchLoginFailure = (error) => ({
+  type: appActions.LOGIN_FAILURE,
+  payload: error
+})
+
+
+/**
+ * Wraps all of our dispatch functions in one. Attempts to check our login 
+ * information with the backend.
+ */
+export function fetchLogin() {
+  return (dispatch) => {
+    // Notify store we're attempting to login
+    dispatch(fetchLoginLoading());
+
+    // Set up options
+    let uri = process.env.REACT_APP_REDIRECT_URI + 'auth/validate';
+    let options = {
+      credentials: 'include',
+      method: 'GET',
+      mode: 'no-cors'
+    };
+
+    rp(uri, options)
+      .then(res => res.json())
+      .then(data => dispatch(fetchLoginSuccess(data)))
+      .catch(err => dispatch(fetchLoginFailure(err)));
+  }
+}
 
 
 /**
--- src/reducers/app.reducer.js
+++ src/reducers/app.reducer.js
@@ -9,14 +9,24 @@ export const appReducer = (state=initialStore.app, action) => {
   let type = action.type;
   
   switch (type) {
-    case (appActions.UPDATE_LOGIN):
-      return {...state, app: {...state.app, loggedIn: action.value }};
-    
     case (appActions.UPDATE_STEP):
-      return {...state, app: {...state.app, step: action.step }};
+      return {...state.app, step: action.step };
 
     case (appActions.SUBMIT_FORM):
-      return {...state, app: {...state.app, save_status: 'SAVING' }};
+      return {...state.app, save_status: 'SAVING' };
+
+    case (appActions.LOGIN_LOADING):
+      return {...state, login_loading: true};
+
+    case (appActions.LOGIN_SUCESS):
+      var response = action.payload;
+      console.log(response);
+      return {...state, loggedIn: response.loggedIn, login_loading: false };
+
+    case (appActions.LOGIN_FAILURE):
+      var err = action.payload;
+      console.log(err);
+      return {...state, login_loading: false, login_error: err };
 
     default:
       return state;
