a16458ef824442feac846f125c7fdc9bb24d6769
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-24 16:57:46 -0500

    Add a simple dumb component

--- src/components/app.js
+++ src/components/app.js
@@ -2,36 +2,27 @@
 import React from 'react';
 import { Fabric } from 'office-ui-fabric-react';
 
-// For Redux
-import { connect } from 'react-redux';
-import { fetchRooms } from '../actions/room.actions';
-import { initialStore } from '../store/initialStore';
-
 
 // Dumb component
-class AppComponent extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      loggedIn: props.loggedIn,
-      rooms: props.rooms,
-      room_error: props.room_error,
-      step: props.step
-    }
-  }
-
-  componentDidMount() {
-    this.props.fetchRooms();
-    console.log(this.props);
-  }
-
+export default class App extends React.Component {
   render() {
     return (
       <Fabric className="App ms-normalize">
         <div className="ms-Grid">
+
           <div className="ms-Grid-row">
             <div className="ms-Grid-col ms-sm12">
               <h1>TESTING</h1>
+            </div>
+          </div>
+
+          <div className="ms-Grid-row">
+            <div className="ms-Grid-col ms-sm6">
+              <h2>TESTING</h2>
+            </div>
+
+            <div className="ms-Grid-col ms-sm6">
+              <h2>TESTING</h2>
             </div>
           </div>
         </div>
@@ -39,16 +30,3 @@ class AppComponent extends React.Component {
     );
   }
 }
-
-
-// Smart
-const mapStateToProps = state => ({ 
-  ...state.appReducer,
-  ...state.roomReducer
-});
-
-const mapDispatchToProps = dispatch => ({
-  fetchRooms: () => dispatch(fetchRooms())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(AppComponent);
