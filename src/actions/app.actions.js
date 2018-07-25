8b645b84244820e942e375fc7f9bf7c5c0749bbb
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-25 16:19:42 -0500

    Add app's user action creators

--- src/actions/app.actions.js
+++ src/actions/app.actions.js
@@ -45,9 +45,8 @@ export function fetchLogin() {
     // Set up options
     let uri = process.env.REACT_APP_REDIRECT_URI + 'auth/validate';
     let options = {
-      credentials: 'include',
-      method: 'GET',
-      mode: 'no-cors'
+      withCredentials: true,
+      method: 'GET'
     };
 
     rp(uri, options)
@@ -69,8 +68,60 @@ export const updateStep = (step) => ({
 
 
 /**
- * Submits our form data to the server.
+ * Initiates submitting our form data to the server.
  */
 export const submitForm = () => ({
   type: appActions.SUBMIT_FORM
 })
+
+
+/**
+ * Notifies application we're initiating a user information API call
+ */
+const fetchUserLoading = () => ({
+  type: appActions.USER_LOADING
+})
+
+
+/**
+ * Notifies application of error during user information API call
+ * @param {object} error Error returned from API
+ */
+const fetchUserError = (error) => ({
+  type   : appActions.USER_ERROR,
+  payload: error
+})
+
+
+/**
+ * Updates store with user information from our API call
+ * @param {object} response Payload with user's email and admin boolean
+ */
+const fetchUserSuccess = (response) => ({
+  type   : appActions.USER_SUCCESS,
+  payload: response
+})
+
+
+/**
+ * Wraps our dispatches together to retrieve user information from our server
+ */
+export function getUser() {
+  return (dispatch) => {
+    // Notify start
+    dispatch(fetchUserLoading());
+
+    // URI + options for API call
+    let uri = `${URI}users/`;
+    let options = {
+      method: 'GET',
+      withCredentials: true
+    };
+
+    // Resolve promise
+    rp(uri, options)
+      .then(res => res.json())
+      .then(data => dispatch(fetchUserSuccess(data)))
+      .catch(err => dispatch(fetchUserError(err)));
+  }
+}
