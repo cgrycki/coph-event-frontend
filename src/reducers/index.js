33dfb39ec03b58e867879eb41e2cfb09bd8bf586
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-25 15:40:03 -0500

    Intergrate event reducer into app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,13 @@ import { appReducer } from './app.reducer';
 import { editorReducer } from './editor.reducer';
 import { fieldReducer } from './field.reducer';
 import { roomReducer } from './room.reducer';
+import { eventReducer } from './events.reducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
   app   : appReducer,
   editor: editorReducer,
   fields: fieldReducer,
-  rooms : roomReducer
+  rooms : roomReducer,
+  events: eventReducer
 });
