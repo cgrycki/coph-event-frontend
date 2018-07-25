8a247715b2bd76c674412018d7de540ac3ff66a1
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-25 16:44:55 -0500

    Remove duplicate grids, add a stub for the third step in our form wizard

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,50 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import { Switch, Redirect, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Fabric } from 'office-ui-fabric-react';
 
 
 // Site components
-import Form from './Form';
+import Form  from './Form';
 import Event from './Event';
 import Admin from './Admin';
 
 
 // Steps for our form
-import StepOne from './steps/01/StepOne';
-import StepTwo from './steps/02/StepTwo';
-
+import StepOne    from './steps/01/StepOne';
+import StepTwo    from './steps/02/StepTwo';
+import StepThree  from './steps/03/StepThree';
 
 // Holds our application data store and sets routes up
 const App = ({ store }) => (
   <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route path="/event"  component={Event} />
-        <Route path="/admin"  component={Admin} />
-        <Redirect from="/" exact to="/form/basic" />
-        <Form>
-          <Route path="/form/basic" component={StepOne} />
-          <Route path="/form/user" component={StepTwo} />
-        </Form>
-      </Switch>
-    </Router>
+    <Fabric>
+      <div className="ms-Grid App">
+
+        <div className="ms-Grid-row">
+        </div>
+
+        <div className="ms-Grid-row">
+          <div className="ms-Grid-col">
+
+            <Router>
+              <Switch>
+                <Route path="/event"  component={Event} />
+                <Route path="/admin"  component={Admin} />
+                <Redirect from="/" exact to="/form/basic" />
+                <Form>
+                  <Route path="/form/basic" component={StepOne} />
+                  <Route path="/form/user"  component={StepTwo} />
+                  <Route path="/form/event" component={StepThree} />
+                </Form>
+              </Switch>
+            </Router>
+
+          </div>
+        </div>
+
+      </div>
+    </Fabric>
   </Provider>
 );
 export default App;
--- src/components/steps/03/StepThree.js
+++ src/components/steps/03/StepThree.js
@@ -3,7 +3,18 @@ import { connect } from 'react-redux';
 
 // Actions
 
+
 // Component
+class StepThreeComponent extends React.Component {
+  constructor(props) {
+    super();
+    this.state = { ...props };
+  }
+
+  render() {
+    return (<div><h3>Step Three</h3></div>);
+  }
+}
 
 
 // Container
@@ -11,4 +22,6 @@ const mapStateToProps = state => ({
   info: state.fields.info,
   errors: state.fields.errors,
   rooms: state.rooms
-})
+});
+
+export default connect(mapStateToProps)(StepThreeComponent);
