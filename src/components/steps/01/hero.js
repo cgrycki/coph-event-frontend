f91d187f8aa0091a59fc2a857ddffcbf188cfdf4
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-26 13:46:54 -0500

    Adjust HERO font styling

--- src/components/steps/01/hero.js
+++ src/components/steps/01/hero.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Hero = () => {
   const hero_styles = {
     'fontWeight'   : '900',
-    'fontFamily'   : 'monospace',
+    'fontFamily'   : 'Arial, sans-serif',
+    'fontVariant'  : 'small-caps',
     'fontSize'     : '120px',
     'lineHeight'   : '0.75',
     'letterSpacing': '12px',
+    'fontStretch'  : 'ultra-expanded',
     'wordSpacing'  : '0',
     'marginTop'    : '-0em',
     'marginBottom' : '50px !important',
