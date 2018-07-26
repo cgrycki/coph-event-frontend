36770d4a7c4488d4b03471ab0cea72ab6f1bebd8
Larson <rlrson@uiowa.edu>
2018-07-25 20:16:19 -0500

    Add a text field component

---
+++ src/components/common/TextField.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TextField } from 'office-ui-fabric-react';
+
+/**
+ * Renders a text field for form inputs.
+ */
+export default class TextInput extends React.PureComponent {
+  render() {
+    let { 
+      label, placeholder, value, onChange,
+      field, error,
+      required, multiline, rows
+    } = this.props;
+
+    return (
+      <TextField
+        // Info and behavior
+        label={this.props.label}
+        placeholder={this.props.placeholder}
+        value={this.props.value}
+        onChange={(evt) => this.props.onChange(evt)}
+
+        /* Error messages and validation. */
+        errorMessage={this.props.error}
+        onGetErrorMessage={(evt) => this.props.onChange(field, evt)}
+        validateOnLoad={false}
+        validateOnFocusOut={true}
+
+        /* Default field behavior */
+        required={this.props.required || true}
+        multiline={this.props.multiline || false}
+        rows={this.props.rows || 1}
+      />
+    );
+  }
+}
