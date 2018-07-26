3eec2e4a287444fb51b290e5bdb26eb96b60a709
Larson <rlrson@uiowa.edu>
2018-07-25 19:24:44 -0500

    Flesh out StepTwo stub with static HTML

--- src/components/steps/02/StepTwo.js
+++ src/components/steps/02/StepTwo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { updateStep } from '../../../actions/app.actions';
+import { DefaultButton } from 'office-ui-fabric-react';
 import { updateField, resetField } from '../../../actions/field.actions';
 
 
@@ -11,10 +11,44 @@ class StepTwoComponent extends React.Component {
     this.state = { ...props };
   }
 
+  prevPage() { this.props.history.push("/form/"); }
+  nextPage() { this.props.history.push("/form/event"); }
+
   render() {
     return (
       <div>
-        <h3>Step Two: User Information</h3>
+        <div className="ms-Grid-row">
+          <div className="ms-Grid-col ms-sm12">
+            <p className="ms-fontSize-xl">User Information</p>
+          </div>
+        </div>
+
+        <div className="ms-Grid-row">
+          <div className="ms-Grid-col ms-sm12">
+            <p>User Email</p>
+            <p>Contact Name</p>
+          </div>
+        </div>
+
+        <div className="FormButtons ms-Grid-row">
+          <div className="ms-Grid-col ms-sm3 ms-smPush1">
+            <DefaultButton
+              primary={false}
+              secondaryText="with your Iowa account."
+              disabled={this.props.loggedIn}
+              onClick={() => this.prevPage()}
+            >Back</DefaultButton>
+          </div>
+
+          <div className="ms-Grid-col ms-sm3 ms-smPush5">
+            <DefaultButton
+              primary={true}
+              secondaryText="Login to create event."
+              disabled={this.props.loggedIn}
+              onClick={() => this.nextPage()}
+            >Next</DefaultButton>
+          </div>
+        </div>
       </div>
     );
   }
