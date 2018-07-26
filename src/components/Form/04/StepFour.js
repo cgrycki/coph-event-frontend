944b1f44f6582cedb50d8d25ed1b2cd3526a017d
Larson <rlrson@uiowa.edu>
2018-07-26 15:30:53 -0500

    Add pager buttons to step four

--- src/components/Form/04/StepFour.js
+++ src/components/Form/04/StepFour.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { connect } from 'react-redux';
-
-// actions
+import FormButtons from '../FormButtons';
 
 
 // React Component
-class StepFourComponent extends React.Component {
+export default class StepFour extends React.Component {
   constructor(props) {
     super();
     this.state = { ...props };
+
+    this.prevPage = this.prevPage.bind(this);
+    this.nextPage = this.nextPage.bind(this);
+  }
+
+  prevPage() {
+    this.props.history.push("/form/event");
+  }
+
+  nextPage() {
+    this.props.history.push("/form/review");
   }
 
   render() {
-    return (<div><h3>Step Four: Layout</h3></div>);
+    return (
+      <div>
+        <h3>Step Four: Layout</h3>
+        <FormButtons
+          prevPage={this.prevPage}
+          nextPage={this.nextPage}
+          disabled={false}
+        />
+      </div>
+    );
   }
 }
-
-
-// Redux Container
-const mapStateToProps = state => ({
-  
-});
-export default connect(mapStateToProps)(StepFourComponent);
