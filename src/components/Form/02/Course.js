e21d99fcf46eceb79dc04927cc6f3533ad29498d
Larson <rlrson@uiowa.edu>
2018-07-27 20:49:00 -0500

    Add course search input (no actions)

--- src/components/Form/02/Course.js
+++ src/components/Form/02/Course.js
@@ -1,50 +1,61 @@
 import React from 'react';
-import { Toggle } from 'office-ui-fabric-react';
+import { Toggle, ComboBox } from 'office-ui-fabric-react';
 import { isNumeric, isLength } from 'validator';
 
 
 /**
- * 
+ * Renders the Course Search input row and dropdown
  */
-class CourseSearch extends React.PureComponent {
-  /* 
-    Fields 
-    FUND
-    ORG
-    DEPT
-    SUB DEPT
-    GRANT PROGRAM
-    INST ACCT
-    ORG ACCT 
-    DEPT ACCT 
-    FUNC COST CNTR
-  */
-  render() {
-    return (<p>Course Search</p>)
-  }
-}
-
 export default class Course extends React.PureComponent {
+  renderSearchPlaceholder() {
+    return { key: 'dropdownPlaceholder', text: 'Start typing a course name' };
+  }
+
+  renderSearch() {
+    // Renders the dropdown/search input to select a class
+    const search_styles = { 
+      "marginLeft": "auto",
+      "width"     : "350px",
+      "maxWidth"  : "350px"
+    };
+
+    return (
+      <div style={search_styles}>
+        <ComboBox
+          label={'Course Name'}
+          allowFreeform={true}
+          autoComplete="off"
+          defaultSelectedKey={this.renderSearchPlaceholder().key}
+          options={[this.renderSearchPlaceholder()]}
+          dropdownWidth={350}
+        />
+      </div>
+    );
+  }
+
+
+
+
   render() {
+    // Styles the row 
     const setup_styles = {
-      "paddingLeft": "8px",
-      "paddingRight": "8px"
+      "padding"       : "0px 8px",
+      "boxSizing"     : "border-box",
+      "display"       : "flex",
+      "justifyContent": "flex-start",
+      "flexDirection" : "row"
     }
+
     return (
       <div className="ms-Grid-row" style={setup_styles}>
         <Toggle
           defaultChecked={false}
-          label={"Event for university course?"}
+          label={"Event is for university course?"}
           onText="True"
           offText="False"
           onChanged={(evt) => this.props.onChange('references_course', evt)}
         />
-        {this.props.references_course && 
-          <CourseSearch
-            onChanged={this.props.onChange}
-            setup_mfk={this.props.setup_mfk}
-          />
-        }
+        {this.props.references_course && this.renderSearch()}
       </div>
     );
   }
