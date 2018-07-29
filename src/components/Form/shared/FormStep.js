8d4f86ea91cf3561814f7e99472c3d74ad80e7ef
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-29 05:18:58 -0500

    Add styling to form step component

--- src/components/Form/shared/FormStep.js
+++ src/components/Form/shared/FormStep.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 export default class FormStep extends React.PureComponent {
   render() {
+    const form_styles = {
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between"
+    };
+
     return (
       <div className="ms-Grid-row">
-        <div className="ms-Grid-col ms-sm12">
+        <div className="ms-Grid-col" style={form_styles}>
           {this.props.children}
         </div>
       </div>
