4624d4b3d82b61addf2c1998af595e1ae3227382
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-28 23:39:45 -0500

    Remove old form and components.

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,29 +1,15 @@
-// For React
 import React from 'react';
 
-// Center the form page within the page container
-const centered_form_style = "ms-Grid-col ms-sm12 ms-lg10 ms-xxl8 ms-lgPush1 ms-xxlPush2";
+import FormTitle from './shared/FormTitle';
+import FormButtons from './shared/FormButtons';
 
-// Dumb component
+
 export default class Form extends React.PureComponent {
   render() {
     return (
-      <div className="Form ms-Grid-row">
-        <div>
-
-          <div className="ms-Grid-row">
-            <div className={centered_form_style}>
-              <h5><strong>Create an Event</strong></h5>
-              <h5 className="ms-textAlignCenter">~~~~~~~~~~~~~~~~~~ Progress Bar ~~~~~~~~~~~~~~~~~~~~~~~</h5>
-            </div>
-          </div>
-
-          <div className="ms-Grid-row">
-            <div className={centered_form_style}>
-              {this.props.children}
-            </div>
-          </div>
-
+      <div className="ms-Grid-row">
+        <div className="ms-Grid-col ms-sm12">
+          {this.props.children}
         </div>
       </div>
     );
