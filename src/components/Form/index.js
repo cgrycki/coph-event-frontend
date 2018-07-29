d7edee538fd023c799d909b781f4700ca4d68dc9
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-29 04:59:19 -0500

    Add styling to make the form expand to the width of the Page component (-padding)

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,8 +4,12 @@ import React from 'react';
 // Component
 export default class Form extends React.PureComponent {
   render() {
+    const form_style = {
+      width: "100%"
+    };
+
     return (
-      <div className="ms-Grid-row" style={{width: '100%'}}>
+      <div className="ms-Grid-row" style={form_style}>
         {this.props.children}
       </div>
     );
