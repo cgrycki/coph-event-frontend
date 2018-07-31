a5d8aa6ef6f0bef01c2952f7f8b18a8af13984c1
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-30 19:21:31 -0500

    Add error message to Course search

--- src/components/Form/fields/Course.js
+++ src/components/Form/fields/Course.js
@@ -13,6 +13,8 @@ export default class Course extends React.PureComponent {
 
   renderSearch() {
     // Renders the dropdown/search input to select a class
+    let { error } = this.props;
+
     const search_styles = { 
       "marginLeft": "auto",
       "width"     : "50%"
@@ -27,14 +29,12 @@ export default class Course extends React.PureComponent {
           selectedKey={this.props.referenced_course}
           defaultSelectedKey={this.renderSearchPlaceholder().key}
           options={[this.renderSearchPlaceholder()]}
-          dropdownWidth={350}
+          errorMessage={error}
+          useComboBoxAsMenuWidth={true}
         />
       </div>
     );
   }
-
-
-
 
   render() {
     let { references_course, onChange } = this.props;
