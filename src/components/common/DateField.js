7d8ec0a0e76ec71f09834447b6da353aab8d66c8
Larson <rlrson@uiowa.edu>
2018-07-31 17:50:44 -0500

    Add several changes to the date field: updated min/max and parsing

--- src/components/common/DateField.js
+++ src/components/common/DateField.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React          from 'react';
 import { DatePicker } from 'office-ui-fabric-react';
-import { nextWeek, datePickerStrings, isWeekend } from '../../utils/date.utils';
-import TextField from './TextField';
+import { 
+  sixMonthsFromToday, 
+  getDateISO, 
+  datePickerStrings, 
+  isWeekend 
+}                     from '../../utils/date.utils';
+import TextField      from './TextField';
 
 
 export default class DateField extends React.PureComponent {
+  parseDate(str) {
+    /* Parses a JS date string into an ISO 8601 format before updating field. */
+    let parsed = getDateISO(str);
+    this.props.onChange('date', parsed);
+  }
+
   renderCophEmail(value, error, onChange) {
     return (
       <div>
@@ -26,18 +37,22 @@ export default class DateField extends React.PureComponent {
       coph_email, coph_email_error,
       onChange
     } = this.props;
+
+    // Conditionally set the value of the date picker
+    const date_value = (date !== "") ? new Date(date) : undefined;
     
     return (
       <div style={{'display': 'inline-block', 'width': '50%'}}>
         <DatePicker
           label={'Event Date'}
           placeholder={'Add a date for the event'} 
-          value={date}
-          minDate={nextWeek()}
+          value={date_value}
+          minDate={new Date()}
+          maxDate={sixMonthsFromToday()}
           isRequired={true}
           showGoToToday={false}
           strings={datePickerStrings}
-          onSelectDate={(evt) => onChange('date', evt)}
+          onSelectDate={(evt) => this.parseDate(evt)}
         />
         {isWeekend(date) && 
           this.renderCophEmail(coph_email, coph_email_error, onChange)}
