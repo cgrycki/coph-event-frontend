35dcdcff09f34197380b41cc226627d61929e169
Larson <rlrson@uiowa.edu>
2018-07-31 19:34:49 -0500

    Flesh out params stub

---
+++ src/utils/param.utils.js
@@ -0,0 +1,34 @@
+/**
+ * Parameter validation
+ */
+
+import { check } from 'validator';
+
+
+export const validUserEmail = () => null;
+
+export const validContactEmail = () => null;
+
+export const validCophEmail = () => null;
+
+export const validEventName = () => null;
+
+export const validEventComments = () => null;
+
+export const validDate = () => null;
+
+export const validStartTime = () => null;
+
+export const validEndTime = () => null;
+
+export const validRoomNumber = () => null;
+
+export const validCourseReference = () => null;
+
+export const validSetupMFK = () => null;
+
+export const validFoodProvider = () => null;
+
+export const validDrinkProvider = () => null;
+
+export const validNumberPeople = () => null;
