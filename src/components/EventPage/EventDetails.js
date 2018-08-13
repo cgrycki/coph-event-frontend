4364c4a701ed81a9337b604325135402949f965a
Larson <rlrson@uiowa.edu>
2018-08-13 15:41:03 -0500

    Add event page progress.

--- src/components/EventPage/EventDetails.js
+++ src/components/EventPage/EventDetails.js
@@ -2,19 +2,158 @@
  * Event Details Component. Responsible for rendering event information.
  */
 
-import React from 'react';
-import { Label } from 'office-ui-fabric-react';
+import React          from 'react';
+import { 
+  Label,
+  Toggle 
+}                     from 'office-ui-fabric-react';
+import { getDateISO } from '../../utils/date.utils';
+import './EventPage.css';
 
-export default class EventDetails extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {};
+export default class EventDetails extends React.PureComponent {
+  renderLabel(text) {
+    /* Renders label with semilight font weight and secondary colored text. */
+    return (
+      <Label className="ms-fontWeight-semilight ms-fontColor-neutralSecondaryAlt">
+        {text}:{' '}
+      </Label>
+    );
   }
 
+  renderHeader(name, room) {
+    /* Renders an overview header. */
+    return (
+      <div className="EventPageHeader">
+        <div>
+          {this.renderLabel("Event Name")}
+          {name}
+        </div>
+
+        <div>
+          {this.renderLabel("Room Number")}
+          {room}
+        </div>
+      </div>
+    );
+  }
+
+  renderDateTimes(date, start, end) {
+    /* Renders row to display date and times. */
+    
+    return (
+      <div className="EventPageDateTimes">
+        <div>
+          {this.renderLabel("Date")}
+          {getDateISO(date)}
+        </div>
+
+        <div>
+          {this.renderLabel("Start")}
+          {start || " "}
+        </div>
+
+        <div>
+          {this.renderLabel("End")}
+          {end || " "}
+        </div>
+      </div>
+    );
+  }
+
+  renderProviders(required, food_provider, drink_provider) {
+    /* Renders a row displaying food and drink providers. */
+    
+    return (
+      <div className="EventPageProviders">
+        <div>
+          {this.renderLabel("Providing Food & Drink")}
+          <Toggle
+            onText="Yes"
+            offText="No"
+            disabled={true}
+            defaultValue={required}
+          />
+        </div>
+
+        <div>
+          {this.renderLabel("Food Provider")}
+          {food_provider || "None"}
+        </div>
+        <div>
+          {this.renderLabel("Alcohol Provider")}
+          {drink_provider || "None"}
+        </div>
+      </div>
+    );
+  }
+
+  renderCourse(required, course) {
+    /* Renders row to view course. */
+    return (
+      <div className="EventPageCourse">
+        <div>
+          {this.renderLabel("References Course")}
+          <Toggle
+            onText="Yes"
+            offText="No"
+            disabled={true}
+            defaultValue={required}
+          />
+        </div>
+
+        <div>
+          {this.renderLabel("Course Name")}
+          {course || "None"}
+        </div>
+      </div>
+    );
+  }
+
+
+
   render() {
+    const { event } = this.props;
+
     return (
       <div className="ms-Grid-row">
-      
+        <div className="ms-Grid-col ms-sm12">
+
+          <div className="EventPageRow">
+            {this.renderHeader(
+              event.event_name,
+              event.room_number)}
+          </div>
+          
+          <div className="EventPageRow">
+            {this.renderLabel("On-Site Contact Email")}
+            {event.contact_email}
+          </div>
+
+          <div className="EventPageRow">
+            {this.renderDateTimes(
+              event.date, 
+              event.start_time, 
+              event.end_time)}
+          </div>
+
+          <div className="EventPageRow">
+            {this.renderProviders(
+              event.food_drink_required, 
+              event.food_provider, 
+              event.alcohol_provider)}
+          </div>
+
+          <div className="EventPageRow">
+            {this.renderCourse(
+              event.references_course,
+              event.referenced_course)}
+          </div>
+
+          <div className="EventPageRow">
+            
+          </div>
+
+        </div>
       </div>
     );
   }
