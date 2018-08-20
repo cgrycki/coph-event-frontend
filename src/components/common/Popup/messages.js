04c64866f164518ded3c7d7497f56b70d37289fe
Larson <rlrson@uiowa.edu>
2018-08-20 14:24:06 -0500

    Create a message dict for  easy app-wide use

---
+++ src/components/common/Popup/messages.js
@@ -0,0 +1,41 @@
+/**
+ * Holds our popup's notification messages.
+ * 
+ * Popup.js expects the following properties
+ *  - isVisible
+ *  - yes button onClick event
+ *  - no button onClick event
+ *  - *title*
+ *  - *subText*
+ *  - *Yes button text*
+ *  - *No button text*
+ */
+
+const messages = {
+  submit: {
+    title: "Event Submission",
+    subText: "After you submit your form, it will be entered in the University of Iowa Workflow system for approval by an administrator.",
+    btnTextYes: "Submit",
+    btnTextNo: "Edit Event"
+  },
+  edit  : {
+    title: "Are you sure you want to edit this event?",
+    subText: "This will clear any unsaved progress in the Event Creation form.",
+    btnTextYes: "Edit Event",
+    btnTextNo: "Cancel"
+  },
+  delete: {
+    title: "Are you sure you want to delete this event?",
+    subText: "This will remove the event from Workflow, and can not be undone.",
+    btnTextYes: "Yes, delete this event",
+    btnTextNo: "Cancel"
+  },
+  error : {
+    title: "We're sorry...",
+    subText: "There was an error while processing your request.",
+    btnTextYes: "Retry",
+    btnTextNo: "Close"
+  }
+};
+
+export default messages;
