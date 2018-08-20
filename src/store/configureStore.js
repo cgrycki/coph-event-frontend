6fda745cd24a6d1b58c456c15c32546f0b35771d
Larson <rlrson@uiowa.edu>
2018-08-20 17:02:38 -0500

    Hook up store to browser history per 'connected-react-router' docs

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,45 +2,44 @@
  * Application Store configuration
  */
 import { createStore, applyMiddleware, compose }  from 'redux'
+import { connectRouter, routerMiddleware }        from 'connected-react-router';
 import thunkMiddleware                            from 'redux-thunk'
 import { createLogger }                           from 'redux-logger'
 import initialStore                               from './initialStore';
+import { createBrowserHistory }                   from 'history';
 import rootReducer                                from '../reducers';
 import { getDateISO }                             from '../utils/date.utils.js';
 
-import { createBrowserHistory } from 'history';
-import { connectRouter, routerMiddleware } from 'connected-react-router';
 
 // Logging middleware
 const loggerMiddleware = createLogger({ collapsed: true });
 
 
-// Function to create a store with async + logging
+/**
+ * Creates our application store configured with our initial state and connected to browser's history.
+ * @param {Object} preloadedState - Object with application's store.
+ * @param {Object} browserHistory - Instance of browser history to connect to application.
+ * @returns {Object} store - Our store with browser history, async, and logging middleware.
+ */
 export function configureStore(preloadedState, browserHistory) {
+  // Check if we have DevTools installed
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-  return createStore(
+  // Create the store
+  const store = createStore(
     connectRouter(browserHistory)(rootReducer),
     preloadedState,
     composeEnhancers(
       applyMiddleware(
         routerMiddleware(browserHistory),
         thunkMiddleware,
-        loggerMiddleware
-      )
-    )
-  );
+        loggerMiddleware)));
+    
+  // Dispatch the current date to the store
+  store.dispatch({ type: 'UPDATE_FIELD', field: 'date', value: getDateISO(new Date())});
+
+  return store;
 }
 
-// Default export, configured store populated from our initialStore
-const history = createBrowserHistory();
-const configuredStore = configureStore(initialStore, history);
 
-// Dispatch the current date to the store
-configuredStore.dispatch({ 
-  type: 'UPDATE_FIELD', 
-  field: 'date', 
-  value: getDateISO(new Date())
-});
-
-export default configuredStore;
+export default configureStore;
