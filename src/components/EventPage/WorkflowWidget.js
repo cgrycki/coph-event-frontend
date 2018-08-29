3e4132bf9d1e8d141a650a751d3b74e33af28895
Larson <rlrson@uiowa.edu>
2018-08-29 18:28:13 -0500

    Can finally f**king load the workflow widget

--- src/components/EventPage/WorkflowWidget.js
+++ src/components/EventPage/WorkflowWidget.js
@@ -1,5 +1,7 @@
 /* Dependencies -------------------------------------------------------------*/
 import React from 'react';
+import workflow from './workflow';
+
 
 export default class WorkflowWidgetComponent extends React.Component {
   constructor(props) {
@@ -9,48 +11,49 @@ export default class WorkflowWidgetComponent extends React.Component {
       signatureId: props.signatureId,
       packageId  : props.packageId
     };
+
+    this.loadScript = this.loadScript.bind(this);
   }
 
   componentDidMount() {
-    this.loadScript(this.state.packageId, this.state.signatureId);
-    this.setState({ loaded: true });
+    this.loadScript();
   }
 
   /** Dynamically loads the Workflow script into our React App */
-  loadScript(pid, sid) {
+  loadScript() {
+    const { packageId: pid, signatureId: sid} = this.state;
+
     // Create an object holding our credentials 
     var WorkflowWidget = window.WorkflowWidget || {};
 
     // Assign variables per https://workflow.uiowa.edu/help/article/36/6
-    WorkflowWidget.location     = encodeURIComponent(document.location);
-    WorkflowWidget.package_id   = pid;
-    WorkflowWidget.signature_id = sid;
-    WorkflowWidget.form_id      = process.env.REACT_APP_FORM_ID;
-    WorkflowWidget.client_id    = process.env.REACT_APP_UIOWA_ACCESS_KEY_ID;
-    WorkflowWidget.scope        = process.env.REACT_APP_UIOWA_SCOPES;
-    WorkflowWidget.environment  = process.env.REACT_APP_WF_ENV;
-    //WorkflowWidget.post_sign_void = '{url - optional}';
-    //WorkflowWidget.post_version_mismatch = '{url - optional}';
+    WorkflowWidget.package_id            = pid;
+    WorkflowWidget.signature_id          = sid;
+    WorkflowWidget.form_id               = process.env.REACT_APP_FORM_ID;
+    WorkflowWidget.client_id             = process.env.REACT_APP_UIOWA_ACCESS_KEY_ID;
+    WorkflowWidget.scope                 = process.env.REACT_APP_UIOWA_SCOPES;
+    WorkflowWidget.environment           = process.env.REACT_APP_WF_ENV;
+    WorkflowWidget.post_sign_void        = 'https://localhost:3000/dashboard';
+    //WorkflowWidget.post_version_mismatch = '';
 
     // !IMPORTANT -- Assign the object to the window so loaded script can pick up credentials
     window.WorkflowWidget = WorkflowWidget;
 
     // Create the script 
-    var cacheBuster = new Date().getTime();
+    /*var cacheBuster = new Date().getTime();
     var script = document.createElement('script');
     script.async = true;
+    script.defer = true;
     script.src   = 'https://workflow.uiowa.edu/workflow-widget/workflow.js?ver='+cacheBuster;
     script.type  = 'text/javascript';
 
     // Inject the script into DOM
     document.getElementsByTagName('head')[0].appendChild(script);
+    */
+    workflow(window);
   }
 
   render() {
-    return (
-      <div>
-        <div id='workflowWidgetContainer' style={{border: '1 px solid black'}}></div>
-      </div>
-    );
+    return (<div id='workflowWidgetContainer' style={{border: '1 px solid black'}}></div>);
   }
 }
