192d870df07a33c30d188d59fbefb426fa3cb46e
Larson <rlrson@uiowa.edu>
2018-08-28 20:57:38 -0500

    Better bottom align footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -5,8 +5,10 @@ import { Link } from 'react-router-dom';
 export default class Footer extends React.PureComponent {
   render() {
     return (
-      <div className="ms-textAlignCenter">
-        <Link className="Footer" to="/about">About Us</Link>
+      <div className="FooterWrapper">
+        <div className="Footer">
+          <Link to="/about">About Us</Link>
+        </div>
       </div>
     );
   }
