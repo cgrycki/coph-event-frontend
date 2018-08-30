fd7d74d8894c53da4c2a4389ca1a2f4dbb2e9f6a
Larson <rlrson@uiowa.edu>
2018-08-30 17:35:18 -0500

    Add autotabbing and delayed store updates to Setup component

--- src/components/Form/fields/Setup/MFK.js
+++ src/components/Form/fields/Setup/MFK.js
@@ -56,7 +56,7 @@ const fields = [
     label: 'ORG ACCT',
     field: 'ORGACCT',
     maxLength: 3,
-    required: true
+    required: false
   },
   {
     label: 'DEPT ACCT',
@@ -82,46 +82,81 @@ const fields = [
 export default class MFK extends React.Component {
   constructor(props) {
     super();
-    this.state = { setup_mfk: {...props.setup_mfk} };
-    this.onFieldChange = this.onFieldChange.bind(this);
-  }
-
-  onFieldChange(field, value, maxLength) {
-
-    //
-    let new_setup = { 
-      ...this.state.setup_mfk,
-      [field]: value
+    this.state = { 
+      setup_mfk: { ...props.setup_mfk },
+      focused  : '#MFK--FUND'
     };
-    this.setState({setup_mfk: new_setup})
+
+    this.onFieldChange = this.onFieldChange.bind(this);
+    this.onFieldsBlur  = this.onFieldsBlur.bind(this);
   }
 
-
+  /** Renders the Text Fields with autotabbing */
   render() {
-    // 
-    //const focusEl = ReactDOM.findDOMNode(component as React.ReactInstance)!!.firstChild as Element;
-    
     return (
-      <div className="ms-slideRightIn20 ms-slideLeftOut20" style={row_style}>
-        <FocusZone
-          direction={FocusZoneDirection.horizontal}
-          handleTabKey={TabTypes.inputOnly}
-          isCircularNavigation={false}
-          allowFocusRoot={false}>
+      <FocusZone
+        direction={FocusZoneDirection.horizontal}
+        handleTabKey={TabTypes.inputOnly}
+        isCircularNavigation={false}
+        allowFocusRoot={false}>
+        <div 
+          className="ms-slideRightIn20 ms-slideLeftOut20" 
+          style={row_style}
+          onBlur={this.onFieldsBlur}>
           {fields.map((field, idx) => {
             return (
               <div key={idx}>
                 <TextField
+                  focusElement={el => console.log(el)}
                   label={field.label}
-                  className={`MFK--${field.field}`}
+                  required={field.required}
+                  id={`MFK--${field.field}`}
+                  className='FormSetupInput'
                   maxLength={field.maxLength}
                   value={this.state.setup_mfk[field.field]}
-                  onChange={evt => this.onFieldChange(field.field, evt.target.value, field.maxLength)}
+                  onChange={evt => this.onFieldChange(idx, evt)}
                 />
               </div>);
           })}
-        </FocusZone>
-      </div>
+        </div>
+      </FocusZone>
     );
+  }
+
+  /** Update component data and focus automagically */
+  onFieldChange(idx, evt) {
+    // Get field properties by looking up field in array via index.
+    const field = fields[idx];
+
+    // Check if we should advance inputs
+    if ((evt.target.value.length >= field.maxLength) && (idx+1 < fields.length)) {
+      // Create a ID selector for next input
+      let next_input_field = fields[idx+1].field;
+      let next_input_id    = `#MFK--${next_input_field}`;
+      let next_input       = document.querySelector(next_input_id);
+      next_input.focus();
+    };
+
+    // Update the field regardless
+    let new_setup = { ...this.state.setup_mfk, [field.field]: evt.target.value };
+    this.setState({ setup_mfk: new_setup });
+  }
+
+  /** Event listener that updates our store when the fields row loses focus */
+  onFieldsBlur(evt) {
+    evt.preventDefault();
+
+    // Get the reference to our focus zone
+    let currentTarget = evt.currentTarget;
+
+    // Keep a reference to the component while in timeout function
+    let component = this;
+
+    // Check if our focus zone contains the active element
+    setTimeout(function() {
+      if (!currentTarget.contains(document.activeElement)) {
+        // If it doesn't then we should update our store
+        component.props.onChange('setup_mfk', component.state.setup_mfk);
+      }}, 1);
   }
 }
--- src/components/Form/fields/Setup/Setup.js
+++ src/components/Form/fields/Setup/Setup.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import { 
-  Toggle, 
-  Label, 
-  TextField,
-  FocusTrapZone,
-  FocusZone,
-  FocusZoneDirection,
-  FocusZoneTabbableElements as TabTypes
-} from 'office-ui-fabric-react';
-import MFK from './MFK';
+import React      from 'react';
+import { Toggle } from 'office-ui-fabric-react';
+import MFK        from './MFK';
+
 
 /**
  * Renders the HTML field to input U. Iowa MFK number if setup is required
@@ -30,101 +23,11 @@ import MFK from './MFK';
  *
  */
 export default class Setup extends React.Component {
-  constructor(props) {
+  constructor() {
     super();
-    this.state = {
-      mfk: { ...props.setup_mfk },
-      inputs: {}
-    };
-
-    this.onFieldChange  = this.onFieldChange.bind(this);
-    this.renderField    = this.renderField.bind(this);
-    this.onFieldBlur    = this.onFieldBlur.bind(this);
-    this.renderFieldRow = this.renderFieldRow.bind(this);
   }
 
-  /** Sets our component field state from *within* the focus zone. */
-  onFieldChange = function(field, value, maxLength, evt) {
-    //console.log(this.refs, evt, evt.target, evt.handle);
-
-    const new_mfk = { ...this.state.mfk, [field]: value };
-
-    // Every update check if the field can be tabbed
-    if (value.length >= maxLength) {
-      //console.log(value, maxLength);
-      // find the html element
-    };
-
-    this.setState({ mfk: new_mfk });
-  }
-  
-  /** Renders a controlled length text field */
-  renderField(field, field_length, required) {
-    // Concat label to match our store's field keys (mapped to props/state)
-    const store_field = field.split(' ').join('');
-    const field_value = this.state.mfk[store_field];
-    
-    return (
-      <div className={"FormSetupInput"}>
-        <TextField
-          label={field}
-          required={required}
-          value={field_value}
-          maxLength={field_length}
-          inputClassName={"FormSetup"}
-          onChange={evt => this.onFieldChange(store_field, evt.target.value, field_length, evt)}
-        />
-      </div>
-    );
-  }
-
-  /** Callback on focus trap zone loses focus. Updates store with comp. state. */
-  onFieldBlur() {
-    this.props.onChange('setup_mfk', this.state.mfk);
-  }
-
-  /** Renders the fields within a row. */
-  renderFieldRow() {
-    // Styles the 'hidden' span that holds our MFK inputs
-    const flex_styles = {
-      "marginLeft"    : "auto",
-      "display"       : "flex",
-      "justifyContent": "space-between",
-      "alignItems"    : "flex-end"
-    };
-    
-    return (
-      <FocusTrapZone>
-        <FocusZone
-          //focusElement={null}
-          direction={FocusZoneDirection.horizontal}
-          handleTabKey={TabTypes.inputOnly}
-          isCircularNavigation={false}
-          allowFocusRoot={false}>
-        <form onBlur={() => this.onFieldBlur()}>
-        <div 
-          className="ms-slideRightIn20 ms-slideLeftOut20"
-          style={flex_styles}>
-            {this.renderField('FUND',       3, true)}
-            {this.renderField('ORG',        2, true)}
-            {this.renderField('DEPT',       4, true)}
-            {this.renderField('SUB DEPT',   5, false)}
-            {this.renderField('GRANT',      8, false)}
-            {this.renderField('INST ACCT',  4, true)}
-            {this.renderField('ORG ACCT',   3, false)}
-            {this.renderField('DEPT ACCT',  5, false)}
-            {this.renderField('FUNC',       2, true)}
-            {this.renderField('COST CNTR',  4, false)}
-          </div>
-          </form>
-        </FocusZone>
-      </FocusTrapZone>
-    );
-  }
-  
   render() {
-    // Renders the setup required row
-    
     // Styles the row 
     const setup_styles = {
       "padding"       : "0px 8px",
@@ -141,12 +44,11 @@ export default class Setup extends React.Component {
           label={"Furniture and setup required?"}
           onText="Yes"
           offText="No"
-          onChanged={(evt) => this.props.onChange('setup_required', evt)}
-        />
-        {this.props.setup_required && <this.renderFieldRow/>}
-        {this.props.setup_required &&
-
-        <p></p>}
+          onChanged={(evt) => this.props.onChange('setup_required', evt)}/>
+        {!this.props.setup_required && 
+          <MFK 
+            setup_mfk={this.props.setup_mfk} 
+            onChange={this.props.onChange} />}
       </div>
     );
   }
