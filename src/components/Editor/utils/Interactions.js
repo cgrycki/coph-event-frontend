62d0293fbb7efd7cdc173958f08693c24ae35b8a
Larson <rlrson@uiowa.edu>
2018-08-31 21:08:01 -0500

    Add initial components and directory structure for Editor (!!)

---
+++ src/components/Editor/utils/Interactions.js
@@ -0,0 +1,22 @@
+/**
+ * Interactions for editor
+ */
+
+// mouse over
+
+// mouse out
+
+// left click
+
+// right click
+
+// drag start (furniture)
+
+// drag end + drop (furniture)
+
+// drag + drop (canvas)
+
+// mousewheel + zoom
+
+
+
