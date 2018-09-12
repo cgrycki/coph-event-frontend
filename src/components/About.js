2e4adfeb577bd0337b2679ddddc2edb994590039
Larson <rlrson@uiowa.edu>
2018-09-11 20:00:43 -0500

    Add stub links to About page

--- src/components/About.js
+++ src/components/About.js
@@ -7,28 +7,28 @@ class About extends React.PureComponent {
   render() {
     return (
       <div className="About">
+
         <div className="ms-Grid-row">
           <div className="ms-Grid-col ms-sm12 ms-md12 ms-lg12 ms-xl12 ms-xxl12">
-            
-            <div className="ms-Grid-row">
-              <div className="ms-Grid-col ms-sm12 ms-md12 ms-lg12 ms-xl12 ms-xxl12">
-                <NavPage history={this.props.history} />
-              </div>
-            </div>
+            <h1>About</h1>
+          </div>
+        </div>
 
-            <div className="ms-Grid-row">
-              <div className="ms-Grid-col ms-sm12 ms-md12 ms-lg12 ms-xl12 ms-xxl12">
-                <h1>About Us</h1>
-              </div>
-            </div>
+        <div className="ms-Grid-row">
+          <div className="ms-Grid-col ms-sm12 ms-md12 ms-lg12 ms-xl12 ms-xxl12">
+            <h3>Links</h3>
+            <ul>
+              <li><a href="https://www.public-health.uiowa.edu/it/">Information Technology Home Page</a></li>
+              <li>Like how we're doing? Hate it? Either way, please let us know and fill out our <a href="https://www.public-health.uiowa.edu/it-customer-satisfaction/">survey.</a></li>
+            </ul>
+          </div>
+        </div>
 
-            <div className="ms-Grid-row">
-
-            </div>
+        <div className="ms-Grid-row">
+          <div className="ms-Grid-col ms-sm12 ms-md12 ms-lg12 ms-xl12 ms-xxl12">
           </div>
         </div>
       </div>
-      
     );
   }
 }
