d3c4d6bc0009abe23379e12138b662f54220ce29
Larson <rlrson@uiowa.edu>
2018-09-17 22:08:21 -0500

    Remove old src folder from first iteration.

--- old-src/constants/index.js
+++
@@ -1,7 +0,0 @@
-import { formActions, editorActions, itemActions, toolbarActions } from './actionTypes';
-import { furnitureTypes, fieldTypes, styleTypes } from './objTypes';
-
-export {
-  formActions, editorActions, itemActions, toolbarActions,
-  furnitureTypes, fieldTypes, styleTypes
-};
--- old-src/containers/index.js
+++
@@ -1,6 +0,0 @@
-import FormContainer from './form.container';
-import GuiContainer from './gui.container';
-import ToolbarContainer from './toolbar.container';
-import HudContainer from './hud.container';
-
-export { FormContainer, GuiContainer, ToolbarContainer, HudContainer };
