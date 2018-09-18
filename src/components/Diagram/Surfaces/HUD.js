2ec3fe04a6109f93b06b013f583817349553762e
Larson <rlrson@uiowa.edu>
2018-09-18 18:23:13 -0500

    Add inventory counts to HUD

--- src/components/Diagram/Surfaces/HUD.js
+++ src/components/Diagram/Surfaces/HUD.js
@@ -1,5 +1,7 @@
 import React           from 'react';
 import { Group, Text } from 'react-konva';
+import inventory       from '../../../constants/inventory';
+
 
 export default class HUD extends React.Component {
   /** Transforms our furniture counts objects into a list of items */
@@ -13,16 +15,17 @@ export default class HUD extends React.Component {
       trash   : 'Trash Cans'
     };
 
-    const left = `Chairs\nCircular Tables\nRectangular Tables\nCocktail Tables\nDisplay Boards\nTrash Cans`;
-    const right = `${counts.chair}\n${counts.circle}\n${counts.rect}\n${counts.cocktail}\n${counts.display}\n${counts.trash}`;
+    const left   = `Chairs\nCircular Tables\nRectangular Tables\nCocktail Tables\nDisplay Boards\nTrash Cans`;
+    const center = `${counts.chair}\n${counts.circle}\n${counts.rect}\n${counts.cocktail}\n${counts.display}\n${counts.trash}`;
+    const right  = `/ ${inventory.chair}\n/ ${inventory.circle}\n/ ${inventory.rect}\n/ ${inventory.cocktail}\n/ ${inventory.display}\n/ ${inventory.trash}`;
 
 
-    return { left, right };
+    return { left, center, right };
   }
 
   render() {
     const { counts, height } = this.props;
-    const { left, right }    = this.createCountText(counts);
+    const { left, center, right }    = this.createCountText(counts);
     const y                  = height * 0.85;
 
     return (
@@ -35,12 +38,21 @@ export default class HUD extends React.Component {
           lineHeight={1.25}
         />
         <Text
-          text={right}
+          text={center}
           align="left"
           fontSize={14}
           fontFamily="monospace"
           lineHeight={1.25}
           offsetX={-150}
+          opacity={0.8}
+        />
+        <Text
+          text={right}
+          align="left"
+          fontSize={14}
+          fontFamily="monospace"
+          lineHeight={1.25}
+          offsetX={-165}
           opacity={0.8}
         />
       </Group>
---
+++ src/constants/inventory.js
@@ -0,0 +1,10 @@
+const inventory = {
+  circle  : 24,
+  rect    : 24,
+  chair   : 192,
+  cocktail: 10,
+  display : 15,
+  trash   : 6
+};
+
+export default inventory;
