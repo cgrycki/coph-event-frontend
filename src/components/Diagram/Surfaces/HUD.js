14958e5412c905abed55803c3a979d9d980a06e6
Larson <rlrson@uiowa.edu>
2018-09-18 20:40:41 -0500

    Fix up HUD to stay in place on canvas

--- src/components/Diagram/Surfaces/HUD.js
+++ src/components/Diagram/Surfaces/HUD.js
@@ -1,5 +1,9 @@
 import React           from 'react';
-import { Group, Text } from 'react-konva';
+import {
+  Group,
+  Text,
+  Rect
+} from 'react-konva';
 import inventory       from '../../../constants/inventory';
 
 
@@ -10,50 +14,38 @@ export default class HUD extends React.Component {
       chair   : 'Chairs',
       circle  : 'Circular Tables',
       rect    : 'Rectangular Tables',
-      cocktail: 'Bar Top (cocktail style) Tables',
+      cocktail: 'Bar Top Tables',
       display : 'Display Boards',
       trash   : 'Trash Cans'
     };
 
-    const left   = `Chairs\nCircular Tables\nRectangular Tables\nCocktail Tables\nDisplay Boards\nTrash Cans`;
-    const center = `${counts.chair}\n${counts.circle}\n${counts.rect}\n${counts.cocktail}\n${counts.display}\n${counts.trash}`;
-    const right  = `/ ${inventory.chair}\n/ ${inventory.circle}\n/ ${inventory.rect}\n/ ${inventory.cocktail}\n/ ${inventory.display}\n/ ${inventory.trash}`;
+    const chairText    = `${labels.chair}             ${counts.chair}\t/\t${inventory.chair}\n`;
+    const circleText   = `${labels.circle}    ${counts.circle}\t/\t${inventory.circle}\n`;
+    const rectText     = `${labels.rect} ${counts.rect}\t/\t${inventory.rect}\n`;
+    const cocktailText = `${labels.cocktail}     ${counts.cocktail}\t/\t${inventory.cocktail}\n`;
+    const displayText  = `${labels.display}     ${counts.display}\t/\t${inventory.display}\n`;
+    const trashText    = `${labels.trash}         ${counts.trash}\t/\t${inventory.trash}\n`;
+    const hudText = chairText + circleText + rectText + cocktailText + displayText + trashText;
 
-
-    return { left, center, right };
+    return hudText;
   }
 
   render() {
-    const { counts, height } = this.props;
-    const { left, center, right }    = this.createCountText(counts);
-    const y                  = height * 0.85;
+    const { counts, height, x, y, scaleX, scaleY }      = this.props;
+    const hudText = this.createCountText(counts);
 
     return (
-      <Group x={50} y={y} >
+      <Group
+        x={(-x/scaleX) + (15 / scaleX)}
+        y={(-y/scaleY) +(0.85*height / scaleY)}
+        scaleX={1}
+        scaleY={1}>
         <Text
-          text={left}
-          align="right"
-          fontSize={14}
-          fontFamily="monospace"
-          lineHeight={1.25}
-        />
-        <Text
-          text={center}
+          text={hudText}
           align="left"
-          fontSize={14}
+          fontSize={14 / scaleX}
           fontFamily="monospace"
           lineHeight={1.25}
-          offsetX={-150}
-          opacity={0.8}
-        />
-        <Text
-          text={right}
-          align="left"
-          fontSize={14}
-          fontFamily="monospace"
-          lineHeight={1.25}
-          offsetX={-165}
-          opacity={0.8}
         />
       </Group>
     );
