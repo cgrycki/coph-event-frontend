33edf872f3d5a502022b1386d03e925cc0e71ab7
Larson <rlrson@uiowa.edu>
2018-09-23 21:32:29 -0500

    Fix floorplan+breadcrumb alignment

--- src/components/Diagram/Playground.js
+++ src/components/Diagram/Playground.js
@@ -54,20 +54,17 @@ class Playground extends Component {
 
   render() {
     return (
-      <div className="ms-Grid-col ms-sm12">
-
+      <div className="ms-Grid-col ms-sm12 ms-xxl12">
         <div className="ms-Grid-row">
-          <div className="ms-Grid-col ms-sm10 ms-xl9">
+          <div className="ms-Grid-col ms-sm10 ms-xl8">
             <p>To save your furniture layout, you'll have to {this.getLink()}.</p>
             <p>Place furniture items and visualize your event by clicking in the {indicator} polygon. You can move and rotate furniture by clicking and dragging. For more instructions, click the help button located below. {pointer}</p>
           </div>
         </div>
 
-
         <div className="ms-Grid-row">
           <Diagram draggable={true} />
         </div>
-
       </div>
     );
   }
