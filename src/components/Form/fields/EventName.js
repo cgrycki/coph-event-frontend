bd971b481c16ea34d6b65ca92348ec4ef3b7ca35
Larson <rlrson@uiowa.edu>
2018-09-28 12:57:34 -0500

    Better event name validation

--- src/components/Form/fields/EventName.js
+++ src/components/Form/fields/EventName.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import TextField from '../../common/TextField';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
 
-export default class EventName extends React.PureComponent {
+
+export default class EventName extends React.Component {
   render() {
-    let { value, error, onChange } = this.props;
+    const { value, error, onChange } = this.props;
 
     return (
       <div className="FormFieldRow">
         <TextField
           label={"Event Name"}
           placeholder={"Add a title for the event"}
-          field={"event_name"}
           value={value}
-          onChange={onChange}
-          error={error}
+          onChange={evt => onChange('event_name', evt.target.value)}
+          errorMessage={error}
           autofocus={true}
         />
       </div>
