b38149c3967648ddefab8fd95cc3c9aca36ba143
IOWA\cgrycki <christopher-grycki@uiowa.edu>
2018-10-25 19:19:34 -0500

    add some more stuff back in

--- src/components/Dashboard/AdminTools.js
+++ src/components/Dashboard/AdminTools.js
@@ -4,5 +4,28 @@ import {PrimaryButton}  from 'office-ui-fabric-react/lib/Button';
 
 
 export default class AdminTools extends React.Component {
+  state = {
+    workflow: '',
+    dynamo  : '',
+    approve : '',
+    void    : ''
+  }
+
+  /** Lifecycle method: resets text fields on successful delete. */
+  componentWillReceiveProps(nextProps) {
+    // Get current store state passed from dashboard
+    const wasLoading = this.props.loading;
+    const isLoading  = nextProps.loading;
+    const isError    = nextProps.error;
+
+    if (wasLoading && !isLoading && !isError) {
+      this.setState({ workflow: '', dynamo: ''});
+    }
+  }
+
+  /** Updates component state upon text field change */
+  updateField = (field, value) => {
+    this.setState({ [field]: value });
+  }
 
 }
